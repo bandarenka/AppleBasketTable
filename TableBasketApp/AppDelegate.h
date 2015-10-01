66e2e3e5f1bc00a00256d8ec62325febc1dd69e6
Admin <admin@Mac-Admin.local>
2015-10-01 14:59:44 +0300

    Initial Commit

---
+++ TableBasketApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableBasketApp
+//
+//  Created by Admin on 01.10.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
